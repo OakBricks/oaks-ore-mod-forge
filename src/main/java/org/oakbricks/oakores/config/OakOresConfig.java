d4e2cbd12d9b4576ae370ce8aba4eb25fc5ea6a8
OakBricks <oakbricks@gmail.com>
2021-07-27 18:07:50 -0500

    change all current int config values to .defineInRange() from .define()

--- src/main/java/org/oakbricks/oakores/config/OakOresConfig.java
+++ src/main/java/org/oakbricks/oakores/config/OakOresConfig.java
@@ -30,24 +30,24 @@ public final class OakOresConfig {
 
         BUILDER.push("Config for OakOres - Purpi Gen");
 
-        purpiVeinSize = BUILDER.comment("Size of a Purpi Ore vein, default value is 6.").worldRestart().defineInRange("Purpi Ore Vein Size", 6, 1, 32);
-        purpiMinHeight = BUILDER.comment("Minimum height for Purpi Ore to generate in a world, default value is 5.").worldRestart().define("Purpi Ore Minimum Height", 5);
-        purpiMaxHeight = BUILDER.comment("Maximum height for Purpi Ore to generate in a world, default value is 28.").worldRestart().define("Purpi Ore Maximum Height", 28);
-        purpiAmount = BUILDER.comment("Amount of Purpi Ore in a world, default value is 10").worldRestart().define("Purpi Ore Amount", 10);
+        purpiVeinSize = BUILDER.comment("Size of a Purpi Ore vein, default value is 6, maximum value of 64.").worldRestart().defineInRange("Purpi Ore Vein Size", 6, 0, 64);
+        purpiMinHeight = BUILDER.comment("Minimum height for Purpi Ore to generate in a world, default value is 5, maximum value of 250.").worldRestart().defineInRange("Purpi Ore Minimum Height", 5, 0, 250);
+        purpiMaxHeight = BUILDER.comment("Maximum height for Purpi Ore to generate in a world, default value is 28, maximum value of 256.").worldRestart().defineInRange("Purpi Ore Maximum Height", 28, 6, 256);
+        purpiAmount = BUILDER.comment("Amount of Purpi Ore in a world, default value is 10, maximum value of 64").worldRestart().defineInRange("Purpi Ore Amount", 10, 0, 64);
 
         BUILDER.pop();
 
         BUILDER.push("Config for OakOres - Lead Gen");
 
-        leadVeinSize = BUILDER.comment("Size of Lead Ore veins, default value is 16").worldRestart().define("Lead Ore Vein Size", 16);
-        leadAmount = BUILDER.comment("Amount of Lead Ore in a world, default value is 25").worldRestart().define("Lead Ore Amount", 25);
+        leadVeinSize = BUILDER.comment("Size of Lead Ore veins, default value is 16").worldRestart().defineInRange("Lead Ore Vein Size", 16, 0, 64);
+        leadAmount = BUILDER.comment("Amount of Lead Ore in a world, default value is 25").worldRestart().defineInRange("Lead Ore Amount", 25, 0, 64);
 
         BUILDER.pop();
 
         BUILDER.push("Config for OakOres - Deepslate Lead Gen");
 
-        deepLeadVeinSize = BUILDER.comment("Size of Deepslate Lead Ore veins, default value is 9").worldRestart().define("Deepslate Lead Ore Vein Size", 9);
-        deepLeadAmount = BUILDER.comment("Amount of Deepslate Lead Ore in a world, default value is 4").worldRestart().define("Deepslate Lead Ore Amount", 4);
+        deepLeadVeinSize = BUILDER.comment("Size of Deepslate Lead Ore veins, default value is 9").worldRestart().defineInRange("Deepslate Lead Ore Vein Size", 9, 0, 64);
+        deepLeadAmount = BUILDER.comment("Amount of Deepslate Lead Ore in a world, default value is 4").worldRestart().defineInRange("Deepslate Lead Ore Amount", 4, 0, 64);
 
 
         BUILDER.pop();
